c4fde5d8a83b34744936d8c787ec468d63cf5728
CAMERON ROBERTS <cameronldroberts@gmail.com>
2020-09-10 20:55:11 +0100

    remove big name

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
-
-
+import mainLogo from "./emoj.jpg"
+import { SocialIcon } from 'react-social-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Cameron Roberts"
+    const siteTitle = ""
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -16,18 +17,19 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
+
         <h1>
-          Hey people, Welcome!{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
+          Hey people, welcome!{" "}
+          <img className="emoji-picture" style={{ width: 100, height: 100, marginBottom: 0 }} src={mainLogo} alt="cam-emoj" />
         </h1>
         <p>
-          My name is Cameron and I'm an IBMer from the UK. I work as a Software Developer and enjoy all things Cloud and IoT. I'm starting a developer blog to document my journey as a developer.
+          My name is Cameron and I'm from the UK. I work as a Software Developer and enjoy all things Cloud and IoT. I'm starting a developer blog to document my journey as a developer.
         </p>
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="35px" marginBottom="10px">Go to Blog</Button>
         </Link>
+        {/* <SocialIcon url="https://twitter.com/cameron_1010" />
+        <SocialIcon url="https://github.com/cameronldroberts" /> */}
       </Layout>
     )
   }
