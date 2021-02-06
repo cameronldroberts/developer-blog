1cdb07e78f9ecf323bc34fdcdd2e4d69299161b0
CAMERON ROBERTS <cameronldroberts@gmail.com>
2021-02-06 11:28:00 +0000

    project page

---
+++ src/components/gitProjects.js
@@ -0,0 +1,108 @@
+
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import axios from 'axios'
+import Typography from '@material-ui/core/Typography';
+import '../css/style.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub
+} from "@fortawesome/free-brands-svg-icons";
+
+
+async function getLanguageStats(repoLanguagesUrl) {
+    let responses = await Promise.all(repoLanguagesUrl.map(url => axios.get(url)));
+    let masterObj = {
+        CSS: 0,
+        Go: 0,
+        HTML: 0,
+        JavaScript: 0,
+        Makefile: 0,
+        Ruby: 0,
+        SCSS: 0,
+        Shell: 0
+    };
+
+    responses.forEach(obj => {
+
+        const keys = Object.keys(obj.data);
+        keys.forEach(key => {
+            const val = obj.data[key];
+            masterObj[key] = (obj.data.hasOwnProperty(key)) ? masterObj[key] + val : val;
+        })
+    })
+
+    console.log(masterObj)
+
+}
+class Projects extends React.Component {
+
+    state = {
+        projects: []
+    };
+
+    componentDidMount() {
+        let repoLanguagesUrl = [];
+        fetch('https://api.github.com/users/cameronldroberts/repos\?sort\=updated\&direction\=desc', {
+        })
+            .then(res => res.json())
+            .then((data) => {
+                data.sort();
+
+                let projectInfo = [];
+                data.forEach(res => {
+                    const obj = {
+                        name: res["name"],
+                        url: res["html_url"],
+                        stars: res["stargazers_count"],
+                        description: res["description"],
+                        date: res["updated_at"]
+                    }
+                    repoLanguagesUrl.push(res["languages_url"])
+                    projectInfo.push(obj);
+                })
+
+                // projectInfo.sort(function (a, b) {
+                //     return new Date(b.date) - new Date(a.date);
+                // });
+                // console.log(JSON.stringify(projectInfo))
+                getLanguageStats(repoLanguagesUrl)
+                this.setState({ projects: projectInfo })
+            })
+            .catch(console.log)
+
+    }
+    render() {
+        return (
+            <div>
+                <Grid container className="empty" spacing={0}>
+                    <Grid item xs={12}>
+                        <Grid container justify="center" spacing={1}>
+                            {this.state.projects.map((value) => (
+                                <Grid key={value} item>
+                                    <Card className="project-card">
+                                        <CardContent >
+                                            <FontAwesomeIcon icon={faGithub} size="2x" />
+                                            <Typography color="textSecondary" >
+                                                <a href={value.url}>{value.name}</a>
+                                            </Typography>
+                                            <Typography className="projectDescription" color="textSecondary">
+                                                {value.description}
+                                            </Typography>
+
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Grid>
+                </Grid >
+            </div>
+        )
+    }
+
+}
+
+export default Projects;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,22 +7,17 @@ import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 import Button from "../components/button"
 import SideBar from "../components/Sidebar";
+import GitProject from "../components/gitProjects"
 class Project extends React.Component {
     render() {
-        const { data } = this.props
-        // const siteTitle = data.site.siteMetadata.title
-        // const posts = data.allMdx.edges
 
         return (
             <div id="App">
                 <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
                 <Layout location={this.props.location}>
                     <SEO title="Projects" />
-                    <h1> Projects</h1>
-                    <p> Page coming soon...</p>
-                    {/* <Link to="/">
-                        <Button marginTop="85px">Go Home</Button>
-                    </Link> */}
+                    <h1> The project wall </h1>
+                    <GitProject />
                 </Layout>
             </div>
         )
